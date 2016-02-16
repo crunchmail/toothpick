3d616c1fdcb06bef57fbf65d92ed1dd38f86eb83
Thomas LEBEAU <tlebeau@oasiswork.fr>
2016-02-16 17:41:26 +0100

    add crunchFitBlock style

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ var license_opts = {tiny: false, organization: 'Oasiswork'};
 gulp.task("minify", function() {
     gulp.src('toothpick.css')
     .pipe(plugins.cssnano())
-    .pipe(plugins.license('MPL', license_opts))
     .pipe(plugins.rename({
         suffix: '.min'
     }))
